5d2caa1c461a5d8d17cd4ba9f8517f4136aa9e0c
shad0waryan <aryan1910044@akgec.ac.in>
2025-07-23 17:35:38 +0530

    added_resume

--- src/app/rightSection.tsx
+++ src/app/rightSection.tsx
@@ -10,6 +10,23 @@ export default function RightSection() {
     >
       <About />
       <Experience />
+      <a
+        href="/resume.pdf"
+        target="_blank"
+        className="flex justify-between items-center bg-[#0f172a] hover:bg-[#1e293b] transition-colors duration-500 p-4 rounded-xl border border-gray-800 hover:border-gray-600 cursor-pointer opacity-60 hover:opacity-100  hover:scale-105"
+      >
+        <h2 className="text-2xl opacity-100 font-semibold text-gray-100 cursor-pointer select-none">
+          Full Resume
+        </h2>
+        <img
+          src="/top_right.png"
+          alt=""
+          width={16}
+          height={16}
+          className="transition-transform duration-500 group-hover:translate-x-1 group-hover:-translate-y-1 "
+        />
+      </a>
+
       <Projects />
     </div>
   );
