27c207ef75dcd7e6676f1d794b5492635af77506
shad0waryan <aryan1910044@akgec.ac.in>
2025-07-23 18:30:51 +0530

    finished_code

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // devIndicators: true,
+  devIndicators: false,
 };
 
 module.exports = nextConfig;
--- src/app/Projects.tsx
+++ src/app/Projects.tsx
@@ -13,21 +13,30 @@ export default function Projects() {
       >
         <ProjectCard
           image="/shad0wbucks.png"
-          title="shad0wbucks"
+          title="SHAD0WBUCKS"
           link="https://shad0wbucks.netlify.app/"
-          description="Video course that teaches how to build a web app with the Spotify Web API. Topics covered include REST APIs, user auth flows, Node, Express, React, Styled Components, and more."
+          description="Frontend clone of Starbucks built with React featuring dynamic routing (React Router DOM), smooth product carousels (Swiper.js), and responsive design (Tailwind CSS). Includes essential e-commerce features like add to cart, item deletion, and secure email/password login via Firebase Auth."
         />
+
         <ProjectCard
-          image="/shad0wbucks.png"
-          title="shad0wbucks"
-          link="https://shad0wbucks.netlify.app/"
-          description="Video course that teaches how to build a web app with the Spotify Web API. Topics covered include REST APIs, user auth flows, Node, Express, React, Styled Components, and more."
+          image="/fluxbasket.png"
+          title="FLUXBASKET"
+          link="https://github.com/shad0waryan/fluxbasket"
+          description="Clean, intuitive UI/UX with a fully functional cart management system. Leverages Next.js features like file-based routing and server-side rendering for performance, demonstrating component-based architecture and stateful logic."
         />
+
         <ProjectCard
-          image="/shad0wbucks.png"
-          title="shad0wbucks"
-          link="https://shad0wbucks.netlify.app/"
-          description="Video course that teaches how to build a web app with the Spotify Web API. Topics covered include REST APIs, user auth flows, Node, Express, React, Styled Components, and more."
+          image="/triviatap.png"
+          title="TRIVIATAP"
+          link="https://github.com/shad0waryan/triviatap"
+          description="Interactive quiz application built with React and dynamic state management. Features conditional rendering based on quiz progress and user interactions, with final score calculation and display for a seamless experience."
+        />
+
+        <ProjectCard
+          image="/xoclash.png"
+          title="XOCLASH"
+          link="https://github.com/shad0waryan/xoclash"
+          description="Turn-based Tic-Tac-Toe game, built in C++ with real-time input handling and smooth board rendering via SFML. Highlights object-oriented design principles and responsive gameplay mechanics."
         />
       </div>
     </section>
